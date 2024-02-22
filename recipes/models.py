ce1c431e4d53a04c4cc243ad02696192f53b29e5
Lucas Flauzino <27flauzino@gmail.com>
2024-02-22 13:21:00 -0300

    criando os modelos

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    name = models.CharField(
+        max_length=65
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+
+    class Meta:
+        verbose_name = 'Receita'
+        verbose_name_plural = 'Receitas'
+
+    title = models.CharField(
+        verbose_name='Título',
+        max_length=65,
+    )
+
+    description = models.CharField(
+        verbose_name='Descrição',
+        max_length=255,
+    )
+
+    slug = models.SlugField()
+
+    preparation_time = models.IntegerField(
+        verbose_name='Tempo de preparação'
+    )
+
+    preparation_time_unit = models.CharField(
+        verbose_name='Tempo de preparação unitário',
+        max_length=65,
+    )
+
+    servings = models.IntegerField(
+        verbose_name='Porções'
+    )
+
+    servings_unit = models.CharField(
+        max_length=65,
+    )
+
+    preparation_steps = models.TextField()
+
+    preparation_steps_is_html = models.BooleanField(
+        default=False,
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name='Criado em',
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name='Atualizado em',
+        auto_now=True
+    )
+
+    is_published = models.BooleanField(
+        default=False,
+    )
+
+    cover = models.ImageField(
+        upload_to='recipes/cover/%Y/%m/%d/'
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Categoria'
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Autor'
+    )
+
+    def __str__(self):
+        return self.title
--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from recipes import views
 
 
+app_name = 'recipes'
+
 # urls do app recipes
 urlpatterns = [
     path('', views.index, name='index'),
