c2db17fa8c8ad9af8687c4d1989ae00b77b3d122
Lucas Flauzino <27flauzino@gmail.com>
2024-02-23 15:10:15 -0300

    dando inicio aos testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+
+    def test_recipe_index_url_is_correct(self):
+        index_url = reverse('recipes:index')
+        self.assertEqual(index_url, '/')
