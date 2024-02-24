ca23ff83caf983ddd5e252aa9a070f379d77068d
Lucas Flauzino <27flauzino@gmail.com>
2024-02-24 14:13:08 -0300

    dando continuidade aos tests das views

--- recipes/tests/test_recipes_views.py
+++ recipes/tests/test_recipes_views.py
@@ -31,3 +31,25 @@ class RecipeViewsTest(TestCase):
             )
         )
         self.assertIs(view.func, views.recipe)
+
+    # teste do status code(index.view)
+    def test_recipes_index_view_returns_statuscode_200_OK(self):
+        response = self.client.get(
+            reverse(
+                'recipes:index'
+            )
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    # teste do template(index.view)
+    def test_recipes_index_view_loads_correct_template(self):
+        response = self.client.get(
+            reverse(
+                'recipes:index'
+            )
+        )
+
+        self.assertTemplateUsed(
+            response, 'recipes/index.html'
+            )
