56869db8b3373a751b9531d9318f5700b884cda1
Lucas Flauzino <27flauzino@gmail.com>
2024-03-04 11:39:49 -0300

    adicionando login-logout e melhorando interface do usuario

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm, LoginForm
 
@@ -42,7 +44,7 @@ def register_create(request):
         del (request.session['register_form_data'])
 
         return redirect(
-            'recipes:index'
+            reverse('authors:login')
             )
 
     contexto = {
@@ -76,7 +78,50 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(
-        request,
-        'author/login.html'
+    if not request.POST:
+        raise Http404
+
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(
+                request, 'Logado com sucesso!'
+            )
+            login(request, authenticated_user)
+
+            return redirect('recipes:index')
+
+        messages.error(
+            request, 'Falha ao logar, usuário ou senha inválidos'
+        )
+        return redirect('authors:login')
+
+
+@login_required(
+        login_url='authors:login', redirect_field_name='next'
+        )
+def logout_view(request):
+    if not request.POST:
+        return redirect(
+            reverse('authors:login')
+        )
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(
+            reverse('authors:login')
+        )
+
+    logout(request)
+    messages.success(
+        request, 'Deslogado com sucesso!'
+    )
+
+    return redirect(
+        reverse('authors:login')
     )
