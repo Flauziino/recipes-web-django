19af74b32ded194cf093d2ba823f69adb1bcabba
Lucas Flauzino <27flauzino@gmail.com>
2024-03-12 13:11:06 -0300

    corrigindo o settings

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['34.125.8.53']
 
 
 # Application definition
